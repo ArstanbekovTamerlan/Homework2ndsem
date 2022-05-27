636e30c869d742d3ffa9b72271643d72649961e9
Arstanbekov Tamerlan <90890370+ArstanbekovTamerlan@users.noreply.github.com>
2022-05-27 16:45:08 +0700

    Update part2.py

--- day10/part2.py
+++ day10/part2.py
@@ -1,5 +1,5 @@
 from itertools import groupby
-arraychik = open('input1.txt', 'r'). read()
+arraychik = open('input.txt', 'r'). read()
 file = open('output2.txt', 'w+')
 for i in range(50):
     arraychik = ''.join([str(len(list(g)))+k for k, g in groupby(arraychik)])
